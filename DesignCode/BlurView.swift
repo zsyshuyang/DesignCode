a88e8c81129707ab56b800b0c9dc932478154dc4
zsy <zsyshuyang@qq.com>
2020-02-27 19:18:34 +0800

    Background Blur

---
+++ DesignCode/BlurView.swift
@@ -0,0 +1,39 @@
+//
+//  BlurView.swift
+//  DesignCode
+//
+//  Created by zsy on 2020/2/27.
+//  Copyright © 2020 zsy. All rights reserved.
+//
+
+import SwiftUI
+
+struct BlurView: UIViewRepresentable {
+    typealias UIViewType = UIView
+    var style: UIBlurEffect.Style
+    
+    func makeUIView(context: UIViewRepresentableContext<BlurView>) -> UIView {
+        let view = UIView(frame: CGRect.zero)
+        view.backgroundColor = .clear
+        
+        let blurEffect = UIBlurEffect(style: style)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        view.insertSubview(blurView, at: 0)
+        
+        NSLayoutConstraint.activate([
+            blurView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            blurView.heightAnchor.constraint(equalTo: view.heightAnchor),
+        ])
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<BlurView>) {
+        
+    }
+    
+    
+    
+    
+}
